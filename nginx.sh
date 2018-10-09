2b17d2adf5f240da65e6e268ebe32fe7b7da677e
aaronstuder <aaronstuder@gmail.com>
2018-10-09 16:58:36 -0400

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -6,9 +6,11 @@ lxc network attach lxdbr0 nginx eth0 eth0
 lxc config device set nginx eth0 ipv4.address 10.0.0.2
 lxc restart nginx
 echo "Updating Container..."
-lxc-attach -n nginx -- apt update && apt upgrade -y
+lxc exec nginx -- apt update && apt upgrade -y
 echo "Installing nginx"
-lxc-attach -n nginx -- apt install nginx -y
-echo "##########"
-echo "Complete"
-echo "##########"
+lxc exec nginx -- apt install nginx -y
+echo
+echo "##############################"
+echo "          Complete            "
+echo "##############################"
+echo
