3254956afef7ec1f9465d2d8ad12e8afb6be8da3
aaronstuder <aaronstuder@gmail.com>
2018-10-10 18:44:09 -0400

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -1,33 +1,38 @@
+
+
 #!/bin/bash
+# Bash Menu Script Example
 
-function install-lxd {
-wget https://raw.githubusercontent.com/aaronstuder/lxd/master/lxd-init.sh && chmod a+x lxd-init.sh && ./lxd-init.sh
+if [ -d "/snap/bin/" ];
+then
+LXD_STATUS=Installed
+else
+LXD_STATUS=NotInstalled
+fi
+
+function check_update {
+    echo "Checking update"
+    wget https://raw.githubusercontent.com/aaronstuder/lxd/master/lxd-init.sh && chmod a+x lxd-init.sh && ./lxd-init.sh
 }
 
-function nginx {
-wget https://raw.githubusercontent.com/aaronstuder/lxd/master/nginx.sh && chmod a+x nginx.sh && ./nginx.sh
+function reinstall_theme {
+    echo "Reinstalling theme"
+    wget https://raw.githubusercontent.com/aaronstuder/lxd/master/nginx.sh && chmod a+x nginx.sh && ./nginx.sh
 }
 
+all_done=0
+while (( !all_done )); do
+    options=("Install LXD $LXD_STATUS" "Reinstall theme" "Quit")
 
-function thirdroutine {
- echo "running adams function"
-}
-
-whiptail --title "LXD Setup Scripts" --checklist --separate-output "Choose:" 20 78 15 \
-"Install LXD" "[Installs LXD and Configures blah blah blah]" off \
-"Install Nginx" "Installs Nginx with SSL and Configures a Port Forward to it" off \
-"Adam" "" off 2>results
-
-while read choice
-do
-        case $choice in
-                "Install LXD") install-lxd
-                ;;
-                "Install Nginx") nginx
-                ;;
-                Adam) thirdroutine
-                ;;
-                *)
-                ;;
+    echo "Choose an option: "
+    select opt in "${options[@]}"; do
+        case $REPLY in
+            1) check_update; break ;;
+            2) reinstall_theme; break ;;
+            3) all_done=1; break ;;
+            *) echo "What's that?" ;;
         esac
-done < results
+    done
+done
+
+echo "Bye bye!"
