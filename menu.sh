749a358fec393a55c2cc9d959be2878dc4f7999d
aaronstuder <aaronstuder@gmail.com>
2018-10-10 16:57:53 -0400

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
-# Bash Menu Script Example
 
-PS3='Please enter your choice: '
-options=("Option 1" "Option 2" "Option 3" "Quit")
-select opt in "${options[@]}"
+function install-lxd {
+wget https://raw.githubusercontent.com/aaronstuder/lxd/master/lxd-init.sh && chmod a+x lxd-init.sh && ./lxd-init.sh
+}
+
+function nginx {
+wget https://raw.githubusercontent.com/aaronstuder/lxd/master/nginx.sh && chmod a+x nginx.sh && ./nginx.sh
+}
+
+
+function thirdroutine {
+ echo "running adams function"
+}
+
+whiptail --title "LXD Setup Scripts" --checklist --separate-output "Choose:" 20 78 15 \
+"Install LXD" "[Installs LXD and Configures blah blah blah]" off \
+"Install Nginx" "Installs Nginx with SSL and Configures a Port Forward to it" off \
+"Adam" "" off 2>results
+
+while read choice
 do
-    case $opt in
-        "Option 1")
-            echo "you chose choice 1"
-            ;;
-        "Option 2")
-            echo "you chose choice 2"
-            ;;
-        "Option 3")
-            echo "you chose choice $REPLY which is $opt"
-            ;;
-        "Quit")
-            break
-            ;;
-        *) echo "invalid option $REPLY";;
-    esac
-done
+        case $choice in
+                "Install LXD") install-lxd
+                ;;
+                "Install Nginx") nginx
+                ;;
+                Adam) thirdroutine
+                ;;
+                *)
+                ;;
+        esac
+done < results
