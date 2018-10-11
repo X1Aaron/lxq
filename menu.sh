58b261bf08e23d2f3ade34f0539ab4427986e71e
aaronstuder <aaronstuder@gmail.com>
2018-10-11 00:38:18 -0400

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -1,8 +1,6 @@
-
-
 #!/bin/bash
-# Bash Menu Script Example
 
+# check if LXD is installed
 if [ -d "/snap/bin/" ];
 then
 LXD_STATUS=Installed
@@ -14,20 +12,24 @@ function install_lxc {
     wget https://raw.githubusercontent.com/aaronstuder/lxd/master/lxd-init.sh && chmod a+x lxd-init.sh && ./lxd-init.sh
 }
 
-function reinstall_theme {
-    echo "Reinstalling theme"
+function install_nginx {
+    wget https://raw.githubusercontent.com/aaronstuder/lxd/master/nginx.sh && chmod a+x nginx.sh && ./nginx.sh
+}
+
+function install_nextcloud {
     wget https://raw.githubusercontent.com/aaronstuder/lxd/master/nginx.sh && chmod a+x nginx.sh && ./nginx.sh
 }
 
 all_done=0
 while (( !all_done )); do
-    options=("Install LXD $LXD_STATUS" "Reinstall theme" "Quit")
+    options=("Install LXD $LXD_STATUS" "Install nginx" "Install Nextcloud" "Quit")
 
     echo "Choose an option: "
     select opt in "${options[@]}"; do
         case $REPLY in
             1) install_lxc; break ;;
-            2) reinstall_theme; break ;;
+            2) install_nginx; break ;;
+            3) install_nextcloud; break ;;
             3) all_done=1; break ;;
             *) echo "What's that?" ;;
         esac
