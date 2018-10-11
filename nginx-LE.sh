5be43185f3528b6edbf477b7c0bea09a46fd1311
aaronstuder <aaronstuder@gmail.com>
2018-10-11 17:58:21 -0400

    Update nginx-LE.sh

--- nginx-LE.sh
+++ nginx-LE.sh
@@ -9,10 +9,16 @@ read CF_Key
 echo
 echo CloudFlare Key is $CF_Key
 echo
+echo
+echo -n "What is your Domain Name?"
+read domain_name
+echo
+echo Domain Name is $domain_name
+echo
 echo "Installing acme.sh..."
 curl https://get.acme.sh | sh
 echo "Installing Certs..."
-acme.sh  --issue -d '*.example.com'  --dns dns_cf
+acme.sh  --issue -d '*.$domain_name'  --dns dns_cf
 
 #CF_Key="sdfsdfsdfljlbjkljlkjsdfoiwje"
 #CF_Email="xxxx@sss.com"
