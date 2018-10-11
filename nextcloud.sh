498fc394b65dc8c5cff5f22f4ac58aef0802bc6d
aaronstuder <aaronstuder@gmail.com>
2018-10-11 00:27:27 -0400

    Update nextcloud.sh

--- nextcloud.sh
+++ nextcloud.sh
@@ -1,20 +1,26 @@
+
 #!/bin/bash
 #domain_name=lxd1.net
-#echo "Domain Name = $domain_name 
+#echo "Domain Name = $domain_name
+
+c=nextcloud
+
 echo
 echo "Creating Container nextcloud..."
-lxc launch ubuntu:18.04 nextcloud
+lxc launch ubuntu:18.04 $c
 echo "Setting IP Address"
-lxc network attach lxdbr0 nextcloud eth0 eth0
-lxc config device set nextcloud eth0 ipv4.address 10.0.0.10
-lxc restart nextcloud
+lxc network attach lxdbr0 $c eth0 eth0
+lxc config device set $c eth0 ipv4.address 10.0.0.10
+lxc restart $c
 echo "Updating Container..."
-lxc exec nextcloud -- apt-get update
-lxc exec nextcloud -- apt-get upgrade -y
-echo "Installing nextcloud"
-lxc exec nextcloud -- apt-get install snapd
-lxc exec nextcloud -- snap install nextcloud
-echo "Configuring nginx..." 
-lxc exec nginx -- wget https://raw.githubusercontent.com/aaronstuder/lxd/master/conf/nextcloud.conf -P /etc/nginx/conf.d/
+sleep 3s
+lxc exec $c -- apt-get update
+lxc exec $c -- apt-get upgrade -y
+echo "Installing $c"
+lxc exec $c -- snap install $c
+echo "Configuring nginx..."
+lxc exec nginx -- wget -nc https://raw.githubusercontent.com/aaronstuder/lxd/master/conf/nextcloud.conf -P /etc/nginx/conf.d/
 lxc exec nginx -- systemctl reload nginx
+echo "Waiting 15 Seconds..."
+sleep 15s
 echo Done!
