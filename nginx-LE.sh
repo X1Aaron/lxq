f8f0a8b80ebffc19cab6a13d15cc022b0db649d0
aaronstuder <aaronstuder@gmail.com>
2018-10-11 20:03:07 -0400

    Update nginx-LE.sh

--- nginx-LE.sh
+++ nginx-LE.sh
@@ -2,5 +2,5 @@ apt-get update
 apt-get install software-properties-common
 add-apt-repository ppa:certbot/certbot
 apt-get update
-apt-get install python-certbot-nginx 
+apt-get install certbot
 certbot certonly --manual --preferred-challenges dns -d *.lxd1.net --agree-tos
