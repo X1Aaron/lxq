f3515b6828a2e1a3deaa19f4c6f408239b00a949
aaronstuder <aaronstuder@gmail.com>
2018-10-12 09:31:38 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,2 @@
+wget https://raw.githubusercontent.com/aaronstuder/lxd/master/lxd-init.sh && chmod a+x lxd-init.sh && ./lxd-init.sh
+wget https://raw.githubusercontent.com/aaronstuder/lxd/master/nginx.sh && chmod a+x nginx.sh && ./nginx.sh
