f2225a62f597574efb4c990bfee90b3d7a17329e
aaronstuder <aaronstuder@gmail.com>
2018-10-11 20:01:32 -0400

    Update nginx-LE.sh

--- nginx-LE.sh
+++ nginx-LE.sh
@@ -1,25 +1,6 @@
-echo
-echo -n "What is your CloudFlare Email?"
-read CF_Email
-echo
-echo CloudFlare Email is $CF_Email
-echo
-echo -n "What is your CloudFlare API Key?"
-read CF_Key
-echo
-echo CloudFlare Key is $CF_Key
-echo
-echo
-echo -n "What is your Domain Name?"
-read domain_name
-echo
-echo Domain Name is $domain_name
-echo
-echo "Installing acme.sh..."
-curl https://get.acme.sh | sh
-export PATH=$PATH:~/.acme.sh/
-echo "Installing Certs..."
-acme.sh  --issue -d '*.$domain_name'  --dns dns_cf
-
-
-
+apt-get update
+apt-get install software-properties-common
+add-apt-repository ppa:certbot/certbot
+apt-get update
+apt-get install python-certbot-nginx 
+certbot certonly --manual --preferred-challenges dns -d *.lxd1.net --agree-tos
