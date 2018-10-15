d51581af961d901c92e69c1649758fb20d83f92a
aaronstuder <aaronstuder@gmail.com>
2018-10-15 09:05:34 -0400

    Update nextcloud.sh

--- nextcloud.sh
+++ nextcloud.sh
@@ -2,22 +2,23 @@
 #!/bin/bash
 echo "Enter your FQDN [Fully Qualified Domain Name]"
 read FQDN
-#domain_name=lxd1.net
-#echo "Domain Name = $domain_name
 
 c=nextcloud
 
 echo
-echo "Creating Container nextcloud..."
+echo "Creating Container $c..."
 lxc launch ubuntu:18.04 $c
 echo "Getting IP Address"
-lxc list "$c" -c 4 | awk '!/IPV4/{ if ( $2 != "" ) print $2}'
+IP=`lxc list "$c" -c 4 | awk '!/IPV4/{ if ( $2 != "" ) print $2}'`
+IP Address is $IP
 echo "Updating Container..."
 sleep 3s
 lxc exec $c -- apt-get update
 lxc exec $c -- apt-get upgrade -y
 echo "Installing $c"
 lxc exec $c -- snap install $c
+echo "Gererating Certificate"
+
 echo "Configuring nginx..."
 lxc exec nginx -- wget -nc https://raw.githubusercontent.com/aaronstuder/lxd/master/conf/nextcloud.conf -P /etc/nginx/conf.d/
 lxc exec nginx -- systemctl reload nginx
