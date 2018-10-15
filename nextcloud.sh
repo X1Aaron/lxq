0139ae31d0c4a91d2fcb94bc7d0742910f51b7d7
aaronstuder <aaronstuder@gmail.com>
2018-10-15 08:58:42 -0400

    Update nextcloud.sh

--- nextcloud.sh
+++ nextcloud.sh
@@ -1,5 +1,7 @@
 
 #!/bin/bash
+echo "Enter your FQDN [Fully Qualified Domain Name]"
+read FQDN
 #domain_name=lxd1.net
 #echo "Domain Name = $domain_name
 
@@ -22,3 +24,25 @@ lxc exec nginx -- systemctl reload nginx
 echo "Waiting 15 Seconds..."
 sleep 15s
 echo Done!
+
+cat > nextcloud.conf <<EOF
+
+server {
+        client_max_body_size 40M;
+        listen 443 ssl;
+        server_name nextcloud.lxd1.net;
+        ssl          on;
+        ssl_certificate /etc/letsencrypt/live/lxd1.net/fullchain.pem;
+        ssl_certificate_key /etc/letsencrypt/live/lxd1.net/privkey.pem;
+
+        location / {
+                proxy_set_header X-Real-IP $remote_addr;
+                proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+                proxy_set_header Host $http_host;
+                proxy_set_header X-NginX-Proxy true;
+                proxy_pass http://10.0.0.10:80;
+                proxy_redirect off;
+        }
+}
+
+EOF
