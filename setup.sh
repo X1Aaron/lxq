04a977907582aebbfcb4052eed93c7c17704a8ed
aaronstuder <aaronstuder@gmail.com>
2018-10-15 13:02:02 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -82,10 +82,9 @@ echo LXD Setup Complete!
 echo
 echo "Creating Container [$n]"
 lxc launch ubuntu:18.04 $n
-echo "Setting IP Address"
-lxc network attach lxdbr0 $n eth0 eth0
-lxc config device set $n eth0 ipv4.address 10.0.0.2
-lxc restart $n
+echo "Getting IP Address"
+IP=`lxc list "$c" -c 4 | awk '!/IPV4/{ if ( $2 != "" ) print $2}'`
+IP Address is $IP
 echo "Updating Container..."
 sleep 5s
 lxc exec $n -- apt-get update
@@ -120,7 +119,7 @@ lxc exec nginx -- systemctl reload nginx
 
 echo "Updating iptables to Forward 443 to nginx Container"
 echo
-iptables -t nat -I PREROUTING -i eth0 -p TCP -d $PUBLIC_IP --dport 443 -j DNAT --to-destination 10.0.0.2:443
+iptables -t nat -I PREROUTING -i eth0 -p TCP -d $PUBLIC_IP --dport 443 -j DNAT --to-destination $IP:443
 echo
 echo "Here are your current PREROUTING Rules"
 iptables -t nat -L PREROUTING
