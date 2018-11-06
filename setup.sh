025333e7879091e90424cc09022c894fbc330705
aaronstuder <aaronstuder@gmail.com>
2018-11-06 12:12:01 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,8 +8,6 @@ git clone https://github.com/aaronstuder/lxq.git /opt/lxq
 chmod a+x /opt/lxq/lxq
 echo "export PATH=$PATH:/opt/lxq" >> ~/.profile
 source ~/.profile
-mkdir /etc/lxq/
-cp /opt/lxq/lxq.cfg /etc/lxq/lxq.cfg
 
 echo
 echo "#################"
